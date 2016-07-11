ddabd04134e31b8848158b8dd945de168493342b
MainScientist <fipezoa@uc.cl>
2016-07-11 14:54:30 -0400

    readme.md

--- demo.py
+++ demo.py
@@ -1,10 +1,9 @@
-from validator import get_schema
-from validator.utils import get_json_from_file
+from validator import get_schema, get_schema_from_file
 
 
-file = get_json_from_file("validator/meta_schema.json")
-schema = get_schema(file)
+file = "validator/meta_schema.json"
+schema = get_schema_from_file(file)
 
-print(schema.validate(file))
+print(schema.validate({"type": "string"}))
 
-get_schema({"type": "string"})
+get_schema({"type": 1})
